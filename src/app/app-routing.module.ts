f168442a327297aeed88ccb48d58fc35141415c8
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-02-20 14:01:30 -0500

    Angular files commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {OtherComponent} from './other/other.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { NaviComponent } from './navi/navi.component';
+
+const routes: Routes = [
+  {path:'other',component:OtherComponent},
+  {path:'',component:NaviComponent},
+  {path:'error',component:ErrorComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NameComponent } from './name/name.component';
+import { OtherComponent } from './other/other.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NaviComponent } from './navi/navi.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    NameComponent,
+    OtherComponent,
+    NavbarComponent,
+    HeroComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+    ErrorComponent,
+    NaviComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
